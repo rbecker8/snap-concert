dd5e95d62c1746244d43b374248bee85fea3d58f
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-12 08:27:27 -0600

    Added more information to johns primary info.  Added attitude section with content.  Added likes section with content. Added needs section with content.  Made image of john wider.

--- index.php
+++ index.php
@@ -5,20 +5,36 @@
 		<title>John</title>
 	</head>
 	<body>
-		<h1>John Johnson</h1>
-		<p><img src="./john.jpg" alt="image of john" style="width: 480px; height: 350px;"></p>
-		<h1 class="main-title">Persona</h1>
-		<ul>
-			<li>Name: John Johnson</li>
-			<li>Gender: Male</li>
-			<li>Age: 29</li>
-			<li>Marital Status: Single</li>
-			<li>Profession: Bartender</li>
-			<li>Income: $40k a year</li>
-		</ul>
-
-
-
+		<p><img src="./john.jpg" alt="image of john" style="width: 600px; height: 350px;"></p>
+		<h1>Persona</h1>
+			<ul>
+				<li>Name: John Johnson</li>
+				<li>Gender: Male</li>
+				<li>Age: 29</li>
+				<li>Marital Status: Single</li>
+				<li>Profession: Bartender @ Metropolitan Room</li>
+				<li>Income: $40k a year</li>
+				<li>Current Location: New York, New York</li>
+			</ul>
+		<p>Attitude</p>
+			<ul>
+				<li>John lives and breathes music</li>
+				<li>Loves living in downtown NY where concerts happen almost daily</li>
+				<li>Enjoys sharing his passion for music online with other music lovers</li>
+				<li>Also likes downloading and discovering new music</li>
+			</ul>
+		<p>Likes</p>
+			<ul>
+				<li>Huge concert venues like Madison Square Garden</li>
+				<li>Meeting celebs and rockstars</li>
+				<li>Spending time reviewing music on his phone</li>
+			</ul>
+		<p>Needs</p>
+			<ul>
+				<li>Websites that are straight forward</li>
+				<li>Websites that don't constantly redirect him to additional pages</li>
+				<li>Websites that organize music by genre as well categories like up and comers</li>
+			</ul>
 
 	</body>
 </html>
