7cab0e5b362c795137538ea13c230342818bec87
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-12 08:16:47 -0600

    Added index page with basic info about john.  Also added jpg image of john.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>John</title>
+	</head>
+	<body>
+		<h1>John Johnson</h1>
+		<p><img src="./john.jpg" alt="image of john" style="width: 480px; height: 350px;"></p>
+		<h1 class="main-title">Persona</h1>
+		<ul>
+			<li>Name: John Johnson</li>
+			<li>Gender: Male</li>
+			<li>Age: 29</li>
+			<li>Marital Status: Single</li>
+			<li>Profession: Bartender</li>
+			<li>Income: $40k a year</li>
+		</ul>
+
+
+
+
+	</body>
+</html>
