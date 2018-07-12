cdb1fda361af68ce764319f734a548fbb823e412
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-12 08:54:14 -0600

    Added use case and user story with content

---
+++ use-case.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>User Story & Use Case</title>
+	</head>
+	<body>
+		<h1 class="main-title">User Story</h1>
+		<ul class="center-list">
+			<li>John needs to review venues as he checks them</li>
+		</ul>
+		<h1 class="main-title">Use Case</h1>
+		<ul class="center-list">
+			<li>John navigates to music venue site</li>
+			<li>Site displays venues based off location first</li>
+			<li>John clicks on small venue</li>
+			<li>Site displays a list of venues that hold 100-150 people</li>
+			<li>John finds his bar/venue</li>
+			<li>Site displays Metropolitan Room</li>
+			<li>Tony enjoys reading comments about his bar</li>
+		</ul>
+	</body>
+</html>
